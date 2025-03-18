d5691a5716dbc0e252c29fe618f0d5c3e960ae21
Laura-Salmonsen <balas@aarhus.dk>
2025-03-18 08:39:02 +0100

    with uv

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 """The main file of the robot which will install all requirements in
 a virtual environment and then start the actual process.
 """
@@ -9,8 +10,14 @@ import sys
 script_directory = os.path.dirname(os.path.realpath(__file__))
 os.chdir(script_directory)
 
-subprocess.run("python -m venv .venv", check=True)
-subprocess.run(r'.venv\Scripts\pip install .', check=True)
+# Install uv
+subprocess.run([sys.executable, "-m", "pip", "install", "uv"], check=True)
+
+# Create virtual environment
+subprocess.run(["uv", "venv"], check=True)
+
+# Install packages in the virtual environment
+subprocess.run(["uv", "pip", "install", "."], check=True)
 
 command_args = [r".venv\Scripts\python", "-m", "robot_framework"] + sys.argv[1:]
 
